aa8fd6472edebe5b68a0cc8b5b1623b00ea14850
FrancoSala1 <f.antoniosala@gmail.com>
2024-12-09 01:03:05 -0300

    actualizacion_4.1

--- negocio/negocio_urls.py
+++ negocio/negocio_urls.py
@@ -1,8 +1,7 @@
 from auxiliares.urls_servicios import UrlServicios
 class NegocioUrls:
-
     #Clase para centralizar la lógica de obtención de URLs.
-
+    
     @staticmethod
     def obtener_url_usuarios():
         return UrlServicios.get_users_url()
