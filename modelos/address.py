eda178f12a1fe7c56dea6a9d5bad943d1ff5bfe7
FrancoSala1 <f.antoniosala@gmail.com>
2024-12-15 15:32:44 -0300

    Actualizacion_12

--- modelos/address.py
+++ modelos/address.py
@@ -2,7 +2,7 @@ from .geolo import Geologo
 class Address(Geologo):
     #este modelo es para representar una dirección de usuario.
     def __init__(self, street, suite, city,zipcode, lat, lng):
-        super().__init__(lat, lng)
+        super().__init__(lat, lng) #llamar al constructor de Geolo
         self.street = street
         self.suite = suite
         self.city = city
--- modelos/usua.py
+++ modelos/usua.py
@@ -1,9 +1,12 @@
-from modelos.address import address
-from modelos.company import compañia
+from .address import Address
+from .company import Company
 
-class User:
+class User(Address, Company):
 # Aqui se representara a un usuario.
-    def __init__(self, id, nombre, username, email, address, celular, website, compañia):
+    def __init__(self, id, nombre, username, email, calle, cuidad, address, celular, website, zipcode, company, lat, lng, nombre_compañia, cath_phrase, bs):
+        #llamar a los constructores de las clases.
+        Address.__init__(self, calle, cuidad, zipcode, lat, lng)
+        Company.__init__(self, nombre_compañia, cath_phrase, bs)
         self.id = id
         self.nombre = nombre
         self.username = username
@@ -11,7 +14,7 @@ class User:
         self.address = address
         self.celular = celular
         self.website = website
-        self.compañia = compañia(**compañia)
+        self.compañia = Company(**Company)
 
     def __str__(self):
         return f"Usuario: {self.name} ({self.usermane}), Email: {self.email}"
